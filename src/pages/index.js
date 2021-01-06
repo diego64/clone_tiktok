9f9310e9f9e5961cef285b5fab5bfd309966fc5b
Diego Ferreira <diegoferreira1964@gmail.com>
2021-01-06 18:34:39 -0300

    Start of home development

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import Layout from '../components/Layout';
 
-function Index() {
-    return <Layout>TikTok</Layout>
+function Index({posts =[]}) {
+    return <Layout>
+        <Feed posts={posts}></Feed>
+        <Suggestions></Suggestions>
+    </Layout>
 }
 
 export default Index;
