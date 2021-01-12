a17c9298d31303d7ca9d723ba88c34bede8a9b6d
Diego Ferreira <diegoferreira1964@gmail.com>
2021-01-12 17:56:42 -0300

    Starting the insertion of data always displayed on the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,56 @@
 import Layout from '../components/Layout';
 import Feed from '../components/Feed';
 
-function Index({ posts =[] }) {
+function Index({ posts }) {
     return (
         <Layout>
             <Feed posts={posts}></Feed>
             {/*<Suggestions></Suggestions>*/}
         </Layout>)
+}
+
+export async function getServerSideProps(context) {
+    //Busca de dados no Banco de dados (MongoDB)
+
+        const data = [
+            {
+                id: 1,
+                author: {id: 1, 
+                    avatar: '/images/download2.png', 
+                    name: 'Diego Ferreira', 
+                    username: 'diegof'
+                },
+                desription: 'Descrição',
+                tags: [{title: '#youtube'}, {title:'//#clonetiktok'}],
+                songName: 'musica',
+                videoUrl: 'video',
+                likes: 650,
+                comments: 10,
+                replies: 50
+            },
+
+            {
+                id: 2,
+                author: {id: 1, 
+                    avatar: '/images/download2.png', 
+                    name: 'Diego Ferreira', 
+                    username: 'diegof'
+                },
+                desription: 'Descrição2',
+                tags: [{title: '#youtube'}, {title:'//#clonetiktok'}],
+                songName: 'musica2',
+                videoUrl: 'video2',
+                likes: 650,
+                comments: 10,
+                replies: 50
+            }
+        ]
+
+    return {
+        props: {
+            posts: data,
+        },
+    }
 }
 
 export default Index;
